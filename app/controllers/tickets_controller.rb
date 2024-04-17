9878842405cdfd31224fe218710036818e8c3c96
AJAYJAITWAL <ajayjaitwal3434@gmail.com>
2024-04-17 15:20:10 +0530

    Delete the search action and move the code into index action

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,7 +3,9 @@ class TicketsController < ApplicationController
   before_action :set_ticket, only: [:show, :update, :destroy]
 
   def index
-    @tickets = current_user.tickets
+    query = params[:search]
+    @tickets = query.present? ? search_tickets(query) : current_user.tickets
+
     render json: @tickets
   end
 
@@ -33,28 +35,6 @@ class TicketsController < ApplicationController
     @ticket.destroy
   end
 
-  def search
-    query = params[:search]
-
-    search_query = {
-      query: {
-        bool: {
-          should: [
-            { match_phrase: { title: query } },
-            { match_phrase: { description: query } },
-            { match: { status: query } },
-          ],
-          must: [
-            { match: { user_id: current_user.id } }
-          ]
-        }
-      }
-    }
-
-    @results = Ticket.search(search_query).records
-    render json: @results
-  end
-
   private
 
   def set_ticket
@@ -63,5 +43,24 @@ class TicketsController < ApplicationController
 
   def ticket_params
     params.require(:ticket).permit(:title, :description, :status)
+  end
+
+  def search_tickets(query)
+    search_query = {
+      query: {
+        bool: {
+          should: [
+            { match_phrase: { title: query } },
+            { match_phrase: { description: query } },
+            { match: { status: query } }
+          ],
+          must: [
+            { match: { user_id: current_user.id } }
+          ]
+        }
+      }
+    }
+
+    Ticket.search(search_query).records
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   resources :users
   post '/auth/login', to: 'authentication#login'
 
-  resources :tickets, only: %i[index show create update destroy] do
-    collection do
-      get 'search'
-    end
-  end
+  resources :tickets, only: %i[index show create update destroy]
 end
