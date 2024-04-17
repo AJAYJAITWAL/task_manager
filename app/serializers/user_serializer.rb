9602836693321e68a4c318af3e25306a856d05a1
AJAYJAITWAL <ajayjaitwal3434@gmail.com>
2024-04-17 14:21:02 +0530

    Update minor changes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :email, :role, :ticket_count
 
   def ticket_count
-    object.tickets.count
+    object.tickets.size
   end
 end
